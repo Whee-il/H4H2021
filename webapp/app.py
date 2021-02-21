d9066e75eb7eb85dc33dee70cfc4e4c4640dc5a3
Whee-il <willcockrum5@gmail.com>
2021-02-20 18:31:54 -0800

    All communication channels open

--- webapp/app.py
+++ webapp/app.py
@@ -1,11 +1,19 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
+import zmq
 #import socket
 
 #Flask Crap
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
+
+context = zmq.Context()
+
+#  Socket to talk to server
+print("Connecting to hello world server…")
+socket = context.socket(zmq.REQ)
+socket.connect("tcp://localhost:5555")
 
 @app.route('/', defaults={"instruction": "Go"})
 @app.route('/<instruction>')
@@ -15,6 +23,12 @@ def index(instruction):
 @socketio.event()
 def my_event(message):
     print(message['data'])
+    
+    socket.send_string(message['data'])
+
+    #  Get the reply.
+    ZMQmessage = socket.recv()
+
     emit('my_response', {'data': message['data']})
 
 @socketio.event
